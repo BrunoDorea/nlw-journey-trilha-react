1fc718d090d4605614cdfdbcde299fbc36d0919b
Bruno Henrique <brunodorea@outlook.com.br>
2024-07-09 15:01:20 -0300

    :sparkles: feat: criação do modal de cadastrar atividade

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,12 @@
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCog, X } from "lucide-react";
+import { useState } from "react";
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+  function openCreateActivityModal() { setIsCreateActivityModalOpen(true) }
+  function closeCreateActivityModal() { setIsCreateActivityModalOpen(false) }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-90 shadow-shape flex items-center justify-between">
@@ -31,7 +37,7 @@ export function TripDetailsPage() {
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
             <button
-              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" onClick={openCreateActivityModal}
             >
               <Plus className="size-5" />
               Cadastrar atividade
@@ -98,9 +104,9 @@ export function TripDetailsPage() {
               Cadastrar novo link
             </button>
           </div>
-          
+
           <div className="w-full h-px bg-zinc-800" />
-          
+
           <div className="">
             <div className="space-y-6">
               <h2 className="font-semibold text-xl">Convidados</h2>
@@ -132,6 +138,50 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+                <button type="button" onClick={closeCreateActivityModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400">
+                Todos convidados podem visualizar as atividades.
+              </p>
+            </div>
+
+            <form className="space-y-3" >
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Tag className="text-zinc-400 size-5" />
+                <input
+                  name="title"
+                  placeholder="Qual a atividade?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Calendar className="text-zinc-400 size-5" />
+                <input
+                  type="datetime-local"
+                  name="occurs_at"
+                  placeholder="Data e horário da atividade"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-950 w-full justify-center rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
+                Salvar atividade
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
